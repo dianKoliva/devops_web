a26075703e00d5d411319df05c216ad8c3c5d2f2
dianKoliva <dianpretty01@gmail.com>
2022-07-05 21:01:32 +0200

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,23 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import context from './MyContext'
+import Home from './screens/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'   
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const  [auth,setAuth]=useState(false);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <context.Provider value={{auth,setAuth}}>
+    <Router>
+    <Routes>
+     <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />  */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
+    </Router>
+    </context.Provider>
   )
 }
 
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+<h3 class='text-primary'>Koliva's School</h3>
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse mlinks  ps-4 " id="navbarCollapse">
+            <div class="navbar-nav  text-light ms-auto">
+            
+                <a href="/" class="nav-item me-4 text-light nav-link active">Home</a>
+               
+              
+                <a href="#" class="nav-item me-4 text-light nav-link">Profile</a>
+                <a href="#" class="nav-item me-4 text-light nav-link">Messages</a>
+                <a href="#" class="nav-item me-4 text-light nav-link" tabIndex="-1">Reports</a>
+            </div>
+           
+            <div class="navbar-nav me-4">
+                <a href="/login" class="nav-item nav-link text-light">Login</a>
+            </div>
+         
+           
+        </div>
+    </div>
+</nav>
+
+
+    </div>
+  )
+}
+
+export default Nav
