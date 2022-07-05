7f1e1091aa3c5ecbd39a8ab89d3fe61b7ae66945
dianKoliva <dianpretty01@gmail.com>
2022-07-05 21:14:35 +0200

    new changes

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 4355;
+const bodyParser = require("body-parser");
+const cors = require('cors')
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+//docker initialisation
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        tittle: "My apis",
+        version: "1.0.0"
+    }
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+})
