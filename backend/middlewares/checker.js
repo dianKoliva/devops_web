2e712607c85f64aed04de7430e5531d6f469af51
dianKoliva <dianpretty01@gmail.com>
2022-07-05 21:31:18 +0200

    new changes

---
+++ backend/middlewares/checker.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+module.exports = (req, res, next) => {
+
+    const token = req.headers.authorization;
+    try {
+        const decoded = jwt.verify(token, "trial");
+        req.userData = decoded;
+        next();
+
+
+    } catch (err) {
+        return res.status(401).json({
+            message: "auth failed",
+            error: err
+        })
+    }
+
+}
